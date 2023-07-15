6af40a2ff6c3bdce78b9990ce03844258b07f820
Musoye <oyebamijimustapha44@gmail.com>
2023-07-15 15:14:49 +0100

    feat:minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,49 +3,29 @@
 '''
 
 
-def check(op: int, n: int, val: str) -> int:
-    '''serve as helper function'''
-    if (op > n):
-        return (1)
-    elif (len(val) >= n):
-        return (2)
-    return (0)
-
-
 def minOperations(n: int) -> int:
     '''Calculate the minimum number of operations'''
-    op = 0
-    holder = 'H'
-    copier = ''
-    val = 0
-    if (n <= 1):
-        return (0)
-    for _ in range(2, n + 2):
-        if op < 3:
-            if op == 0:
-                copier = holder
-                op += 1
-            else:
-                holder += copier
-                op += 1
-        else:
-            if n == 4:
-                holder += copier
-                op += 1
-                val += 1
-            else:
-                if val % 2 == 0:
-                    copier = holder
-                    op += 1
-                    val += 1
-                else:
-                    holder += copier
-                    op += 1
-                    val += 1
-        if (check(op, n, holder) == 0):
+    current = 1
+    holder = 0
+    count = 0
+    while (current < n):
+        if holder == 0:
+            holder = current
+            count += 1
+        if current == 1:
+            current += holder
+            count += 1
             continue
-        elif (check(op, n, holder) > 1):
-            return (op)
-        elif (check(op, n, holder) == 1):
-            return (0)
-    return (0)
+        rem = n - current
+        if rem < holder:
+            return 0
+        if rem % current == 0:
+            holder = current
+            current += holder
+            count += 2
+        else:
+            current += holder
+            count += 1
+    if current != n:
+        return 0
+    return count
