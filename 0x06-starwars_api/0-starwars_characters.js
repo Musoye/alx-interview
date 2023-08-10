fe26148c9035a15e089030028e52d5d9a4704e26
Musoye <oyebamijimustapha44@gmail.com>
2023-08-11 00:19:41 +0100

    0. Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,9 +1,26 @@
 #!/usr/bin/node
-const request = require('request')
+// star wars api
+const request = require('request');
 
-function (url, arg) {
-  request(url, (error, response, body) {
-	  console.log(body)
-  }
-}
-
+const star = (url) => {
+  request(url, (error, response, body) => {
+    if (error) {
+      throw error;
+    }
+    const characters = JSON.parse(body);
+    const people = characters.characters;
+    for (const character of people) {
+      request(character, (error, response, body) => {
+        if (error) {
+          throw error;
+        }
+        const person = JSON.parse(body);
+        console.log(person.name);
+      });
+    }
+  });
+};
+
+const value = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + value;
+star(url);
