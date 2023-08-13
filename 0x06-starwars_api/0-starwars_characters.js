5ece4fc44fc87b2da41549993e7e9e3a5a9364b1
Musoye <oyebamijimustapha44@gmail.com>
2023-08-13 18:17:18 +0100

    fixify

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,25 +2,18 @@
 // star wars api
 const reques = require('request');
 const util = require('util');
-const request = util.promisify(reques)
+const request = util.promisify(reques);
 
-const star = (url) => {
-  request(url, (error, response, body) => {
-    if (error) {
-      throw error;
-    }
-    const people = JSON.parse(body).characters;
-    for (const character of people) {
-      request(character, (error, response, body) => {
-        if (error) {
-          throw error;
-        }
-        const person = JSON.parse(body);
-        console.log(person.name);
-      });
-    }
-  });
-};
+async function star (url) {
+  const body = await (await request(url)).body;
+  const pot = JSON.parse(body);
+  const people = pot.characters;
+  for (const character of people) {
+    const bod = await (await request(character)).body;
+    const person = JSON.parse(bod);
+    console.log(person.name);
+  }
+}
 
 const value = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + value;
